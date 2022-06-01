0844965a472473e4ebad949ccfa7dd8a4c561df4
Connor Groen <71343047+ScienceOwl13@users.noreply.github.com>
2022-06-01 21:51:18 +0000

    Basic Bot Functions

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import discord
-from discord.ext import commands
+import os
 
-bot = commands.bot(command_prefix="-")
+client = discord.Client()
 
-@bot
+@client.event
+# When the bot is ready
+async def on_ready():
+	print("Bot have logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	if message.content.startswith("$hello"):
+		await message.channel.send("Hello!")
+
+# client.run(os.environ('TOKEN'))
+
+client.run(os.environ['TOKEN'])
