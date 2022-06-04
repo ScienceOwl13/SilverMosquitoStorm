23b763c8aed9b1230ab28c23bd3f6e7bdba73906
Connor Groen <71343047+ScienceOwl13@users.noreply.github.com>
2022-06-04 23:54:03 +0000

    Adds clear command & `Readme.md` improved

--- main.py
+++ main.py
@@ -6,43 +6,36 @@ from discord.ext import commands
 # Import Libraries for console date/time messages
 from datetime import datetime
 from pytz import timezone 
+import time
 
 client = commands.Bot(command_prefix = "-") # Set the prefix for the bot
 client.remove_command("help") # Remove default help command so custom can be used
 
 # Gets current time for the console command log
-def currentTime():
+def currentTimeEST():
 	Eastern = timezone("US/Eastern")
 	EST_Time = datetime.now(Eastern)
 
-	return EST_Time.strftime("%H:%M:%S EST")
+	return EST_Time.strftime("%H:%M:%S EST") # Formats the time correctly
 
-	
 # When bot starts
 @client.event
 async def on_ready():
 	print("Bot is ready.")
+	print(f"The current time is {currentTimeEST()}")
 	print("Logged in as @{0.user}".format(client))
 	print("----------\n")
-	
-# Atominous bot things
-@client.event
-async def on_member_join(member):
-	print(f"{member} has joined the server! Say hello!")
-@client.event
-async def on_member_remove(member):
-	print(f"{member} doesn't want to stay on the server! Say goodbye!")
-	
 
 # commands
+
 @client.command()
-async def ping(ctx):
-	print(f"Ping command was called at {currentTime()}")
+async def ping(ctx): # Ping command
+	print(f"Ping command was called at {currentTimeEST()} by {ctx.message.author}")
 	await ctx.send(f"Pong! Latency of `{round(client.latency * 1000)}ms`.")	
 
 @client.command()
 async def help(ctx):
-	print(f"Help command was called at {currentTime()}")
+	print(f"Help command was called at {currentTimeEST()} by {ctx.message.author}")
 	
 	embed = discord.Embed(
 		description = "*Please notethat this bot is currently under developement, so there may be bugs. If bugs happen, please let <@!624686837448835115> know.*",
@@ -55,9 +48,15 @@ async def help(ctx):
 
 	await ctx.send(embed=embed)
 
+@client.command()
+async def clear(ctx, amount=5):
+	print(f"Clear command was called at {currentTimeEST()} by {ctx.message.author}")
+	time.sleep(1)
+	await ctx.message.delete()
+	await ctx.channel.purge(limit = amount)	
 	
 # Make bot run without rate limiting
 try:
 	client.run(os.environ['TOKEN'])
 except:
-    os.system("kill 1")
+  os.system("kill 1")
--- readme.md
+++ readme.md
@@ -1,2 +1,20 @@
 # Project SilverMosquitoStorm
-A discord bot created by me/
+A discord bot created by me, Connor Groen.<br>
+Made using [Discord.py](https://github.com/Rapptz/discord.py), this bot is not a very good nor talented bot.
+
+## Commands
+Currently, there aren't many commands that the bot is capable of doing.
+
+**Help**
+<br>
+`-help`: displays a embed that provides the syntax and description of each command.
+
+**Ping**
+<br>
+`-ping`: responds with "pong" while also displaying the latency.
+
+
+**Clear**
+<br>`-clear`: removes the previous five messages in the channel, as well as the message used to initiate the command.
+
+`-clear [number]`: removes the specified amount of messages, as well as the message used to initiate the command.
